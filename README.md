53350cb009044ad92c5131ea1ead9c0e30260cc9
imfht <fiht@qq.com>
2023-12-06 20:39:49 +0800

    add read me and streamlit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WHICH FOOD
+使用CNN识别肉的种类，视频请见：https://www.bilibili.com/video/BV1xN4y1e7gX/
+
+Use cnn detect food type,see detail at https://www.youtube.com/watch?v=0nC1BBnMmnY
+
--- cnn.py
+++ cnn.py
@@ -1,11 +1,12 @@
+import streamlit as st
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from PIL import Image
 from torch.utils.data import random_split, DataLoader
 from torchvision import datasets, transforms
 
-# 定义数据预处理
 # 定义数据预处理
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -42,35 +43,70 @@ class SimpleCNN(nn.Module):
         return x
 
 
-# 初始化模型、损失函数和优化器
-model = SimpleCNN()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+def train():
+    # 初始化模型、损失函数和优化器
+    model = SimpleCNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# 训练模型
-num_epochs = 20
-for epoch in range(num_epochs):
-    model.train()
-    for inputs, labels in train_loader:
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-    # 在验证集上进行评估
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for inputs, labels in val_loader:
+    # 训练模型
+    num_epochs = 20
+    for epoch in range(num_epochs):
+        model.train()
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
             outputs = model(inputs)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-    accuracy = correct / total
-    print(f'Epoch {epoch + 1}/{num_epochs}, Validation Accuracy: {accuracy:.4f}, total: {total}, correct: {correct}')
+        # 在验证集上进行评估
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-# 保存模型
-torch.save(model.state_dict(), 'trained_model.pth')
+        accuracy = correct / total
+        print(
+            f'Epoch {epoch + 1}/{num_epochs}, Validation Accuracy: {accuracy:.4f}, total: {total}, correct: {correct}')
+    # 保存模型
+    torch.save(model.state_dict(), 'trained_model_cnn.pth')
+
+
+def preprocess_image(image):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    image = transform(image).unsqueeze(0)
+    return image
+
+
+if __name__ == '__main__':
+    model = SimpleCNN()
+    model.load_state_dict(torch.load('trained_model_cnn.pth'))
+    st.title('人工智能肉类识别器')
+    st.write('上传图片，开始识别.')
+
+    # 上传用户提供的图像
+    uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'jpeg', 'png'])
+
+    if uploaded_file is not None:
+        # 显示用户上传的图像
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+        # 预测
+        with torch.no_grad():
+            tensor_image = preprocess_image(image)
+            outputs = model(tensor_image)
+            _, predicted = torch.max(outputs, 1)
+            predicted_class = predicted.item()
+        predicted_class = '牛肉' if predicted_class == 0 else '大肉'
+        st.write(f'Predicted Class: {predicted_class}')
